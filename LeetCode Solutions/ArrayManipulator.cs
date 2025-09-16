cfa740aa4303d33c1d998758c2035241da0f52ae
Elias Kolbenschlag <elias.kolbenschlag@rohde-schwarz.com>
2025-09-16 16:32:36 +0200

    Two Sums Problem solved

---
+++ LeetCode Solutions/ArrayManipulator.cs
@@ -0,0 +1,31 @@
+namespace LeetCode_Solutions;
+
+public static class ArrayManipulator
+{
+    /// <summary>
+    /// Solves the <see href="https://leetcode.com/problems/two-sum/description/">Two Sum Problem</see>.
+    /// </summary>
+    /// <param name="nums">An array of integers</param>
+    /// <param name="target">The target integer</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int[] TwoSums(int[] nums, int target)
+    {
+        if (nums is null)
+            throw new ArgumentNullException(nameof(nums));
+        if (nums.Length == 0)
+            throw new ArgumentException("Array cannot be empty", nameof(nums));
+        
+        var numDict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (numDict.TryGetValue(complement, out int value))
+            {
+                return [value, i];
+            }
+            numDict[nums[i]] = i;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
---
+++ LeetCode Unit Tests/ArrayManipulatorTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode_Solutions;
+
+namespace LeetCode_Unit_Tests;
+
+public class ArrayManipulatorTests
+{
+    [Theory]
+    [InlineData(new[] {2,7,11,15},9,new[] {0,1})]
+    [InlineData(new[] {3,2,4},6,new[] {1,2})]
+    [InlineData(new[] {3,3},6,new[] {0,1})]
+    public void TwoSums_ShouldSucceed(int[] nums, int target, int[] expected)
+    {
+        Assert.Equal(ArrayManipulator.TwoSums(nums, target),expected);
+    }
+    
+    [Theory]
+    [InlineData(null,2,typeof(ArgumentNullException))]
+    [InlineData(new int[0],2,typeof(ArgumentException))]
+    [InlineData(new [] {1,2},100,typeof(ArgumentException))]
+    public void TwoSums_ShouldThrow(int[] nums, int target, Type expectedException)
+    {
+        var exception = Record.Exception(() => ArrayManipulator.TwoSums(nums, target));
+        Assert.NotNull(exception);
+        Assert.IsType(expectedException, exception);
+    }
+}
